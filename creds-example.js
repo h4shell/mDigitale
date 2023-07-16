de7f2bde940da2dd1ad90bb7ce0b4269f6494ecb
h4shell <fw1987@gmail.com>
2023-07-17 00:19:03 +0200

    push creds-example.js

---
+++ creds-example.js
@@ -0,0 +1,10 @@
+// creds.js
+
+const creds = {
+    host: '127.0.0.1', // MySql: Ip server
+    user: 'username', // username 
+    password: 'password', //password
+    database: 'menu_digitale', //database name
+  };
+
+module.exports = {creds};

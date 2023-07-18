281c464ffed1cf0b7bfaa84166c086f27609794f
h4shell <fw1987@gmail.com>
2023-07-18 23:51:55 +0200

    fix hash

--- mymodule/hash.js
+++ mymodule/hash.js
@@ -14,6 +14,5 @@ function checkHash(string, hash){
     }   
 }
 
-console.log(checkHash("lorenzo","c37d6c9f47b2f729d6abfda2cedb9d3b8a0f9f8c37a9f7f4f6785c9a7e246e25"))
 
 module.exports = {checkHash, hashGen}
